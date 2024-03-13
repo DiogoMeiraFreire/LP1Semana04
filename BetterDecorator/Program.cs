5e275b1854fe5c1cb2866868d2a2159c83147e65
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-13 09:32:23 +0000

    Improved the project to now retrieve the number of characters asked between the string

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -6,13 +6,28 @@ namespace BetterDecorator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Decor("Good Morning!", '*'));
-            Console.WriteLine(Decor("Good Morning!", '.'));
+            Console.WriteLine(Decor(args[0], 
+                                    char.Parse(args[1]), 
+                                    int.Parse(args[2])));
         }
 
-        private static string Decor (string s, char dec)
+        private static string Decor (string s, char dec, int rep)
         {
-            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+            string result = "";
+
+            for (int i = 0; i <= rep; i++)
+            {
+                result += dec;
+            }
+
+            result += $" {s} ";
+
+            for (int i = 0; i <= rep; i++)
+            {
+                result += dec;
+            }
+
+            return result;
         }
     }
 }
