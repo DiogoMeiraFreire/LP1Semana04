483bbddbc4721f101d2fbf6adee5e21f9df313f4
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-13 12:29:35 +0000

    Completed project Special

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,29 @@ namespace Special
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Insert n-th number of special sequence: ");
+            int count1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(count1));
+        }
+
+        private static int GetSpecial(int count)
+        {
+            int result;
+
+            if (count <= 0)
+            {
+                result = 0;
+            }
+            else if (count == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = GetSpecial(count - 1) + (GetSpecial(count-2) * 2);
+            }
+
+            return result;
         }
     }
 }
